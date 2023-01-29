b18c10686d1cfc80ea375f337c4f94126621c5af
JainitBITW <jainit.bafna@research.iiit.ac.in>
2023-01-29 16:03:10 +0530

    Part1 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import SignUp from './components/SignUp/SignUp';
 import './App.css';
-
+import Login from './components/Login/Login';
+import { Routes, BrowserRouter as Router, Link, Route, useNavigate, Navigate } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Profile from './components/Profile/Profile';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import WithAuth from './components/WithAuth/WithAuth';
+function Auth() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  if ( localStorage.getItem('email')==='admin' && localStorage.getItem('password')==='admin') {
+    return (<Navigate to="/profile"></Navigate>)
+  } 
+  else if (location.search === '?mode=login') {
+    return <Login></Login>
+  }
+  else if (location.search === '?mode=signup') {
+    return <SignUp></SignUp>
+  }
+  else {
+    return (<div>Page not Valid </div>)
+  }
+}
+function Home() {
+  if ( localStorage.getItem('email')==='admin' && localStorage.getItem('password')==='admin') {
+    return (<Navigate to="/profile"></Navigate>)
+  } else {
+    return (<Navigate to="/login?mode=login"></Navigate>)
+  }
+}
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <NavBar></NavBar>
+      <div>
+        <Routes>
+          <Route
+            exact path="/login" element={<Auth></Auth>} ></Route>
+          <Route exact path="/profile" element={<WithAuth Component={Profile}/>} ></Route>
+          <Route exact path="/" element={<Home/>} ></Route>
+
+        </Routes>
+      </div>
+
+    </>
   );
 }
 
